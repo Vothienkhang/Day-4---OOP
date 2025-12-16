7e0b64b892a6c104f24c79ab1b97aab48ce998b3
khangvo95 <khang.vo@getoutabox.com>
2025-12-16 15:48:42 +0700

    Static property

---
+++ src/Main_Car.java
@@ -0,0 +1,10 @@
+public class Main_Car {
+    public static void main(String[] args) {
+        Car car1 = new Car("Mazda 3", "Skyactiv 3");
+        System.out.println(Car.numberOfCars);
+        Car car2 = new Car("Mazda 2", "Skyactiv 2");
+        System.out.println(Car.numberOfCars);
+        Car car3 = new Car("Mazda 6", "Skyactiv 6");
+        System.out.println(Car.numberOfCars);
+    }
+}
