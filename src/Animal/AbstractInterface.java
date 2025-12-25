0b097edf9b18b5397a46fcda445751e011c4e06b
khangvo95 <khang.vo@getoutabox.com>
2025-12-25 19:37:42 +0700

    Edible

--- src/Animal/AbstractInterface.java
+++ src/Animal/AbstractInterface.java
@@ -1,12 +1,22 @@
 package Animal;
 
+import Animal.Animal;
+import Animal.Chicken;
+import Animal.Tiger;
+import Edible.Edible;
+
 public class AbstractInterface {
     public static void main(String[] args) {
         Animal[] animals = new Animal[2];
-        animals[0] = new Chicken();
-        animals[1] = new Tiger();
+        animals[0] = new Tiger();
+        animals[1] = new Chicken();
         for (Animal animal : animals) {
             System.out.println(animal.makeSound());
+
+            if (animal instanceof Chicken) {
+                Edible edible = (Chicken) animal;
+                System.out.println(edible.howToEat());
+            }
         }
     }
 }
--- src/Animal/Chicken.java
+++ src/Animal/Chicken.java
@@ -1,8 +1,16 @@
 package Animal;
 
-public class Chicken extends Animal {
+import Edible.Edible;
+
+public class Chicken extends Animal implements Edible {
     @Override
     public String makeSound() {
         return "Chicken: cluck-cluck!";
+    }
+
+
+    @Override
+public String howToEat() {
+    return "Chicken: could be fried!";
     }
 }
